b59f325375f3f880f4ed804df0f67fccd6f54141
Andrew <drew982@yahoo.com>
2016-12-22 21:37:26 -0600

    Added Node to Linked List

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import linkedlist.LinkedList;
+
 import java.util.Scanner;
 
 /**
@@ -8,8 +10,16 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in); //Pass data into scanner
-        String string = scanner.next(); //Pass data from terminal to program
-        System.out.println(string);
-//        LinkedList head = new LinkedList("Andrew");
+        String string;
+        LinkedList head = new LinkedList("Andrew");
+
+        do
+        {
+            string = scanner.next(); //Pass data from terminal to program
+            LinkedList node = new LinkedList(string); //Holds new string passed in
+            head.add(node);
+        }
+        while (!string.equalsIgnoreCase("q"));
+        scanner.close();
     }
 }
